d43a84e554b51f9fd5e9b07f5ae1395aca24ea99
unknown <JaBi@ORD01572.ordina.belgium>
2015-09-03 11:54:50 +0200

    Profile update

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,13 +8,26 @@
  * Controller of the empApp
  */
 angular.module('empApp')
-  .controller('ProfileCtrl', function ($scope, $log) {
-    
+  .controller('ProfileCtrl', function ($http, $scope, $log) {
+
     $log.info('ProfileCtrl loaded');
-    
+    $scope.errmsg = "hello";
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+
+    $scope.settings = {
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'Male',
+      enrolmentDate: Date.now(),
+      birthDate: Date.now() + 9999999
+    }
+
+    $http.get
   });
