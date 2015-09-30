9291a3499397c1354165883d1c0b67c229f06688
Gert-jan Theunissen <gert-jan.theunissen@ordina.be>
2015-09-30 10:43:03 +0200

    Added the ability to change user's password

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -9,9 +9,9 @@
  */
 angular.module('empApp')
   .controller('SettingsCtrl',SettingCtrl);
-SettingCtrl.$inject = ['$scope','$log','$modal'];
+SettingCtrl.$inject = ['$scope','$log','$modal', '$timeout', 'dataservice'];
 
-function SettingCtrl ($scope, $log, $modal) {
+function SettingCtrl ($scope, $log, $modal, $timeout, dataservice) {
     $log.info('SettingsCtrl loaded');
 
     $scope.modalPasswordReset = function() {
@@ -32,5 +32,32 @@ function SettingCtrl ($scope, $log, $modal) {
         $scope.modal.dismiss('cancel');
       };
     };
+
+  $scope.resetPassword = function(password) {
+
+    var id = window.sessionStorage.getItem("id");
+    var userData  = {
+      'oldPassword' : password.oldPassword,
+      'newPassword' : password.newPassword
+    };
+
+    $scope.passwordChanged = false;
+    $scope.passwordWrong = false;
+
+    var handleSuccess = function(data) {
+      $log.info("Password changed");
+      $scope.passwordChanged = true;
+
+      $timeout(function() {
+        $scope.modal.close();
+      }, 1500);
+    };
+    var handleError = function() {
+      $log.info("Current password not correct");
+      $scope.passwordWrong = true;
+    }
+
+    dataservice.postItem('POST', 'http://localhost:8080/api/persons/' + id + '/settings/resetPassword', userData, 'application/json').success(handleSuccess).error(handleError);
+  }
 
   };
