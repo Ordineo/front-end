56399d69edfa992eab644f58c689885d3f830347
Jeff Mesens <jeff.mesens@gmail.com>
2015-09-30 18:34:10 +0200

    Fixing autoreload on table, added delete button, added category

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -4,30 +4,63 @@ angular.module('empApp')
   .controller('SkillsCtrl', function ($scope, $modal, SkillFactory, DataService, $log) {
 
     $log.info('SkillsCtrl loaded');
-    $scope.skills =  SkillFactory.all();
+    $scope.skills = SkillFactory.all();
 
     $scope.add = function () {
-      $scope.modal = $modal.open({
+      var modalInstance = $modal.open({
         animation: $scope.animationsEnabled,
         templateUrl: 'skillModalContent',
-        controller: 'SkillsCtrl',
-        size: 'lg',
-        scope: $scope
+        controller: 'SkillsModalCtrl',
+        size: 'sm'
       });
-      $scope.modal.result.then(function (message) {
-        console.log(message);
+
+      modalInstance.result.then(function (skill) {
+        console.log('Adding skill...');
+
+        if (skill.skillCategory) {
+          console.log(skill.skillCategory);
+          DataService.postItem('POST', 'http://localhost:8081/api/skillCategories/', skill.skillCategory, 'application/json', function (data,config,headers) {
+            skill.skillCategory = headers('location');
+            DataService.postItem('POST', 'http://localhost:8081/api/skills/', skill, 'application/json', updateList);
+
+          });
+        }else{
+          DataService.postItem('POST', 'http://localhost:8081/api/skills/', skill, 'application/json', updateList);
+        }
+
+
       }, function () {
         console.log('Modal dismissed');
       });
     };
 
+
+    $scope.remove = function (href) {
+      console.log('Removing skill...');
+      SkillFactory.remove(href, updateList);
+    };
+
+    function updateList() {
+      $scope.skills = SkillFactory.all();
+    }
+
+  }).controller('SkillsModalCtrl', function ($scope, $modalInstance, $log) {
+
+    $log.info('SkillsModalCtrl loaded');
+
+    $scope.errorMessage = null;
+
     $scope.ok = function (skill) {
-      console.log('Adding skill...');
+      if (!skill || skill.name.trim().length == 0)
+        $scope.errorMessage = "Enter a name";
+      else {
+        $scope.errorMessage = null;
+        $modalInstance.close(skill);
+      }
+    };
 
-      DataService.postItem('POST','http://localhost:8081/api/skills/',skill,'application/json');
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
 
-      $scope.modal.close();
-
-      };
-
-});
+  });
--- app/scripts/services/skills.service.js
+++ app/scripts/services/skills.service.js
@@ -1,41 +1,62 @@
 'use strict';
 
 angular.module('empApp')
-  .factory('SkillFactory', function ($http) {
+  .factory('SkillFactory', function ($http, DataService, $resource) {
     var skills = [];
-    var getSkills = (function () {
+
+    function getSkills() {
       skills = [];
-      $http.get('http://localhost:8081/api/skills').success(function (data) {
-        if(data._embedded.skills !== undefined) {
-          data._embedded.skills.forEach(function (skill) {
+
+      DataService.getItem('http://localhost:8081/api/skills', function (data) {
+        data = data.data._embedded.skills;
+        if (data !== undefined) {
+          data.forEach(function (skill) {
+            console.log(skill);
+            if (skill._links.skillCategory) {
+
+              var SkillCategory = $resource(skill._links.skillCategory.href);
+              var v = SkillCategory.get().$promise.then(function (data) {
+                skill.category = data.name;
+              });
+            }
+
             skills.push(skill);
           });
         }
       });
-    })();
+    }
+
+    function remove(href, success) {
+      DataService.postItem('DELETE', href, null, null, success);
+    }
 
     return {
       all: function () {
+        getSkills();
         return skills;
       },
-      update: getSkills
+      update: getSkills,
+      remove: remove
+
     };
-  })
-  .factory('DataService', ['$http', function ($http) {
+  }
+)
+  .factory('DataService', function ($http) {
     return {
-      getItem: function (url) {
-        return $http.get(url);
+      getItem: function (url, success) {
+        return $http.get(url).then(success);
       },
 
-      postItem: function (method, url, postdata, headers) {
+      postItem: function (method, url, postdata, contentType, success) {
 
         return $http({
           method: method,
           url: url,
           data: postdata,
-          headers: {'Content-Type': headers}
-        });
+          headers: {'Content-Type': contentType}
+        }).success(success);
+
       }
     };
 
-  }]);
+  });
