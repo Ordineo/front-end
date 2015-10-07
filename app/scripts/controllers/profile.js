d807fc64910853b8e6ffe2cf8d930f2335f7dd9f
Jasper Biesemans <jasperbiesemans@ORD02470.local>
2015-10-07 10:00:19 +0200

    Skill Competences not ready to implement yet

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -111,31 +111,7 @@ function ProfileCtrl($scope, $modal, $log, $http, $location, dataservice, Person
   //Skill competences
   //-----------------
 
-  updateList();
-
-  $scope.save = function (skillCompetence) {
-    console.log('Adding Skill Competence to current user');
-    skillCompetence.skill = skillCompetence.skill._links.self.href;
-    skillCompetence.person = window.sessionStorage.getItem("id");
-    SkillCompetenceFactory.add(skillCompetence, updateList);
-    $scope.skillCompetence = null;
-  };
-
-  $scope.remove = function (href) {
-    SkillCompetenceFactory.remove(href, updateList);
-  };
-
-  SkillFactory.getSkills(function (response) {
-    $scope.skills = response.data._embedded.skills;
-  });
-
-  PersonFactory.getPerson().success(function (data) {
-    $scope.person = data;
-  });
-
-  function updateList() {
-    $scope.skillCompetences = SkillCompetenceFactory.all();
-  }
+  
 
   //-----------
   //Credentials
