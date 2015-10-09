fed2b4be8bb02d71cd513c4d74c7d9c6eb63a957
Jeff Mesens <jeff.mesens@gmail.com>
2015-10-09 10:30:13 +0200

    Added missing dependency

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-material-data-table/dist/md-data-table.min.js',
       'bower_components/angular-material-icons/angular-material-icons.min.js',
+      'bower_components/lodash/lodash.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
