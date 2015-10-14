8ccaee911e0eb7d1649d576b91c37ecf0b61ac4f
Jasper Biesemans <jasperbiesemans@ORD02470.local>
2015-10-14 16:35:19 +0200

    Directive ready: certificates

--- app/scripts/persons/directives/about.js
+++ app/scripts/persons/directives/about.js
@@ -1,12 +1,26 @@
 angular.module('oraj360')
-  .directive('aLittleAboutMe', aLittleAboutMe);
+  .directive('aLittleAboutMe', aLittleAboutMe)
+  .directive('certificates', certificates);
 
 function aLittleAboutMe() {
   var directive = {
     link: link,
     templateUrl: 'views/alittleaboutme.html',
-    restrict: 'E',
+    restrict: 'EA',
     controller: 'ProfileCtrl'
+  };
+  return directive;
+
+  function link(scope, element, attrs) {
+    /* */
+  }
+}
+
+function certificates() {
+  var directive = {
+    link: link,
+    templateUrl: 'views/certificates.html',
+    restrict: 'EA'
   };
   return directive;
 
