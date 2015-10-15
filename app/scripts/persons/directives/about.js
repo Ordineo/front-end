ad11acf77fa52adc9f063314bf5f16762eaf3572
Jasper Biesemans <jasperbiesemans@ORD02470.local>
2015-10-15 14:28:34 +0200

    Directive ready and responsive: interests

--- app/scripts/persons/directives/about.js
+++ app/scripts/persons/directives/about.js
@@ -1,6 +1,7 @@
 angular.module('oraj360')
   .directive('aLittleAboutMe', aLittleAboutMe)
-  .directive('certificates', certificates);
+  .directive('certificates', certificates)
+  .directive('interests', interests);
 
 function aLittleAboutMe() {
   var directive = {
@@ -20,6 +21,19 @@ function certificates() {
   var directive = {
     link: link,
     templateUrl: 'views/certificates.html',
+    restrict: 'EA'
+  };
+  return directive;
+
+  function link(scope, element, attrs) {
+    /* */
+  }
+}
+
+function interests() {
+  var directive = {
+    link: link,
+    templateUrl: 'views/interests.html',
     restrict: 'EA'
   };
   return directive;
