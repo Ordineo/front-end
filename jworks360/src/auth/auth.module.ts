1ea044bfed842c4b128959661551fe5f712467ff
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-02-29 13:49:46 +0100

    added ui-router

--- jworks360/src/auth/auth.module.ts
+++ jworks360/src/auth/auth.module.ts
@@ -3,8 +3,6 @@ import {LOGIN} from "./component/login.directive";
 import {LoginDirective} from "./component/login.directive";
 import {MATERIAL_DESIGN} from "../material-design/material-design";
 import IModule = angular.IModule;
-import {AUTH_SERVICE} from "./component/auth.service";
-import {AuthService} from "./component/auth.service";
 
 export const JWORKS360_AUTH = 'jworks360.auth';
 
@@ -16,6 +14,3 @@ var authModule:IModule = angular.module(JWORKS360_AUTH, deps);
 
 authModule
   .directive(LOGIN, LoginDirective.instance);
-
-authModule
-  .service(AUTH_SERVICE, AuthService);
--- jworks360/src/jworks360.ts
+++ jworks360/src/jworks360.ts
@@ -1,9 +1,14 @@
 import {JWORKS360_AUTH} from "./auth/auth.module";
 import {MATERIAL_DESIGN} from "./material-design/material-design";
+import 'angular-ui-router';
+
 export const APP_NAME = "jworks360";
 
+var deps:Array<string> = [
+  JWORKS360_AUTH,
+  MATERIAL_DESIGN,
+  'ui.router'
+];
+
 angular
-  .module(APP_NAME, [
-    JWORKS360_AUTH,
-    MATERIAL_DESIGN
-  ]);
+  .module(APP_NAME, deps);
