fbb31fab9bcfe4e3873ad33403fe6a0b87dd6e1e
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-03-03 11:51:19 +0100

    added low priority stuff

---
+++ jworks360/src/sugar/sugar.module.ts
@@ -0,0 +1,13 @@
+/**
+ * Module meant for cool stuff, but not necessary.
+ *
+ * */
+require('gsap');
+
+export const JWORKS_SUGAR = "jworks360.sugarcoat";
+
+var deps:Array<string> = [
+  'angular-animate'
+];
+
+angular.module(JWORKS_SUGAR, deps);
