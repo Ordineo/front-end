fcfa5b2b50eacb87bc1db379be9eb578793b2421
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-03-04 11:51:12 +0100

    added sessionstorage to authservice

--- jworks360/src/auth/service/auth.service.ts
+++ jworks360/src/auth/service/auth.service.ts
@@ -1,3 +1,4 @@
+import IWindowService = angular.IWindowService;
 export const AUTH_SERVICE = "jworks360AuthService";
 
 export interface ICredentials {
@@ -7,9 +8,26 @@ export interface ICredentials {
 
 export interface IAuthService {
   validateCredentials(user:ICredentials):boolean;
+  createSession(user:string):void;
+  isAuthorized():boolean;
 }
 
 export class AuthService implements IAuthService{
+  static $inject:Array<string> = ['$window'];
+  static SESSION_KEY = 'user';
+
+  constructor(private $window:IWindowService){
+
+  }
+
+  createSession(user:string):void {
+    this.$window.sessionStorage.setItem(AuthService.SESSION_KEY, 'user');
+  }
+
+  isAuthorized():boolean {
+    return this.$window.sessionStorage.getItem(AuthService.SESSION_KEY);
+  }
+
   validateCredentials(user:ICredentials):boolean {
     return user.email === 'j@d.be';
   }
