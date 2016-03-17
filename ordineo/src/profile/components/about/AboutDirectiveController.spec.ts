b70fb702717ad278daa02b1ab2e457ad63eb06dd
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-03-17 13:41:29 +0100

    Update controller testing

--- ordineo/src/profile/components/about/AboutDirectiveController.spec.ts
+++ ordineo/src/profile/components/about/AboutDirectiveController.spec.ts
@@ -10,6 +10,7 @@ import IControllerService = angular.IControllerService;
 import IRootScopeService = angular.IRootScopeService;
 import IScope = angular.IScope;
 import IDeferred = angular.IDeferred;
+import {IAboutModel} from "./IAboutModel";
 
 describe('About directive controller', ()=> {
   var $controller:IControllerService, $q:IQService, $rootScope:IRootScopeService;
@@ -37,7 +38,24 @@ describe('About directive controller', ()=> {
     });
     it("should not have content loaded", ()=> {
       expect(ctrl.isContentLoaded).toBeFalsy();
-    })
+    });
+    it("should be collapsed", ()=> {
+      expect(ctrl.isCollapsed).toBeTruthy();
+    });
+    it("should have a short description property wich cannot be longer than 365 chars", ()=> {
+      bindings = {
+        description: getMockAboutInfo().description
+      };
+      ctrl = $controller(AboutDirectiveController, {$scope: scope}, bindings);
+      expect(ctrl.shortDescription.length).toBeLessThan(366);
+    });
+    it("should have a short description wich equals the first same 365 characters of the description", ()=> {
+      bindings = {
+        description: getMockAboutInfo().description
+      };
+      ctrl = $controller(AboutDirectiveController, {$scope: scope}, bindings);
+      expect(ctrl.shortDescription).toBe(getMockAboutInfo().description.substr(1, 365));
+    });
   });
 
   describe("when username gets binded to a value", ()=> {
@@ -63,41 +81,49 @@ describe('About directive controller', ()=> {
   });
 
   describe("when the data is fetched from the remote service", ()=> {
+    var deferred:IDeferred<any>;
+
     beforeEach(()=> {
-      var deferred:IDeferred<any> = $q.defer();
+      deferred = $q.defer();
       bindings = {
         username: 'nivek'
       };
       spyOn(serviceMock, 'getAboutInfoByUsername').and.returnValue(deferred.promise);
-
       ctrl = $controller(AboutDirectiveController, {$scope: scope}, bindings);
-
       expect(serviceMock.getAboutInfoByUsername).toHaveBeenCalled();
+    });
 
-      deferred.resolve({
-        function: 'designer',
-        unit: {
-          name: 'jworks'
-        },
-        description: 'hey'
+    describe("and the promise gets resolved", ()=> {
+      beforeEach(()=> {
+        deferred.resolve(getMockAboutInfo());
+        scope.$digest();
       });
-      scope.$digest();
+
+      it('should set isContentLoaded to true', ()=> {
+        expect(ctrl.isContentLoaded).toBeTruthy();
+      });
+      it('should have function equal to designer', ()=> {
+        expect(ctrl.functie).toBe('designer');
+      });
+      it('should have unit equal to jworks', ()=> {
+        expect(ctrl.unit).toBe('jworks');
+      });
+      it('should have matching description ', ()=> {
+        expect(ctrl.description).toBe(getMockAboutInfo().description);
+      });
     });
 
-    it('should have content loaded', ()=> {
-      expect(ctrl.isContentLoaded).toBeTruthy();
-    });
-    it('should have function equal to designer', ()=> {
-      expect(ctrl.functie).toBe('designer');
-    });
-    it('should have unit equal to jworks', ()=> {
-      expect(ctrl.unit).toBe('jworks');
-    });
-    it('should have description equal to hey', ()=> {
-      expect(ctrl.description).toBe('hey');
+    describe("and the promise gets rejected", ()=> {
+      beforeEach(()=> {
+        deferred.reject();
+        scope.$digest();
+      });
+
+      it('should set isContentLoaded to false', ()=> {
+        expect(ctrl.isContentLoaded).toBeFalsy();
+      });
     });
   });
-
   describe("when username does not get binded", ()=> {
     beforeEach(inject((_$rootScope_, _$controller_, _$q_:IQService)=> {
       bindings = {};
@@ -112,3 +138,17 @@ describe('About directive controller', ()=> {
     })
   })
 });
+
+function getMockAboutInfo():any {
+  return {
+    function: 'designer',
+    unit: {
+      name: 'jworks'
+    },
+    description: `Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
+A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way.
+When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then`
+  }
+}
--- ordineo/src/profile/components/about/AboutDirectiveController.ts
+++ ordineo/src/profile/components/about/AboutDirectiveController.ts
@@ -6,21 +6,27 @@ interface IAboutDirective {
   functie:string;
   unit:string;
   description:string;
+  shortDescription:string;
   username:string;
   isContentLoaded:boolean;
   isEditModeEnabled:boolean;
   title:string;
+  aboutInfoCache:IAboutModel;
+  isCollapsed:boolean;
+  setIsCollapsed:Function;
 }
 
 export class AboutDirectiveController implements IAboutDirective {
   public functie:string;
   public unit:string;
+  public shortDescription:string;
   public description:string;
   public isContentLoaded:boolean;
   public isEditModeEnabled:boolean;
   public aboutInfoCache:IAboutModel;
   public title:string;
   public username:string;
+  public isCollapsed:boolean;
 
   static $inject:Array<string> = [
     ProfileService.NAME,
@@ -28,7 +34,8 @@ export class AboutDirectiveController implements IAboutDirective {
 
   constructor(public profileService:IProfileService) {
     this.isContentLoaded = false;
-
+    this.setIsCollapsed(true);
+    this.setInfoCache();
     if (this.username) {
       profileService.getAboutInfoByUsername(this.username)
         .then((data:any)=> {
@@ -38,18 +45,32 @@ export class AboutDirectiveController implements IAboutDirective {
           this.title = data.firstName + ' ' + data.lastName;
           this.isContentLoaded = true;
         }, (error:any)=> {
-          profileService.getMock().then((data:any)=> {
-            this.functie = data.info.functie;
-            this.unit = data.info.unit;
-            this.description = data.info.description;
-            this.isContentLoaded = true;
-          });
+          this.isContentLoaded = false;
         });
     }
   }
 
   onEdit():void {
     this.isEditModeEnabled = !this.isEditModeEnabled;
+    this.aboutInfoCache = {
+      functie: this.functie,
+      description: this.description,
+      unit: this.unit
+    };
+  }
+
+  setIsCollapsed(isCollapsed:boolean):void {
+    this.isCollapsed = isCollapsed;
+    if (this.description) {
+      if (isCollapsed) {
+        this.shortDescription = this.description.substr(1, 365);
+      } else {
+        this.description = this.aboutInfoCache.description;
+      }
+    }
+  }
+
+  private setInfoCache():void {
     this.aboutInfoCache = {
       functie: this.functie,
       description: this.description,
