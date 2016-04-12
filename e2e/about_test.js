76aca3a85cf8ad8a96a6bb4037adeecce587cf44
Christophe Theyssen <christophe.theyssen@ordina.be>
2016-04-12 15:06:43 +0200

    Fixing karma and e2e tests

--- e2e/about_test.js
+++ e2e/about_test.js
@@ -9,12 +9,14 @@ Before((I) => {
 Scenario('Open the profile page', (I) => {
   I.see(pageTitle, '.header-bg');
   I.see('Sync with Linkedin', '#getDetails');
-  I.waitForElement('.about-header', 2);
+  I.waitForElement('.about-header');
   I.seeElement('.about-header');
   //I.seeNumberOfElements('.about-header', 1);
   I.seeElement('.btnEdit');
   I.dontSeeElement('.btnSave');
   I.dontSeeElement('.edit-info');
+  I.waitForElement('.about-footer');
+  I.seeElement('.about-footer');
   I.see('MORE', '.about-footer');
   I.dontSee('COLLAPSE', '.about-footer');
   I.click('.btnMore');
@@ -22,28 +24,30 @@ Scenario('Open the profile page', (I) => {
   I.see('COLLAPSE', '.about-footer');
 });
 
+
 Scenario('Edit about section and cancel', (I) => {
-  I.waitForElement('.btnEdit', 2);
-  I.click('.btnEdit');
-  I.waitForElement({model: 'about.employee.function'});
+  I.waitForElement('.about-header');
+  I.click('.btnEdit', '.about-header');
+  I.waitForElement({model: 'about.employee.function'}, 2);
   I.fillField({model: 'about.employee.function'},'wrong function');
-  I.click('.btnCancel');
+  I.click('.btnCancel', '.about-header');
   I.dontSee( 'wrong function', '.about-function');
 });
 
 Scenario('Edit about section and save changes', (I) => {
-  I.waitForElement('.btnEdit', 2);
-  I.click('.btnEdit');
+  I.waitForElement('.about-header');
+  I.click('.btnEdit', '.about-header');
   I.waitForElement({model: 'about.employee.function'});
   I.fillField({model: 'about.employee.function'},'test function');
   I.fillField({model: 'about.employee.description'},'test description');
   I.fillField({model: 'about.employee.unit.name'},'test unit');
-  I.click('.btnSave');
-  I.waitForElement('.about-header', 2);
+  I.click('.btnSave', '.about-header');
+  I.waitForElement('.about-header');
   I.see( 'test function', '.about-function');
   I.see( 'test unit', '.about-unit');
   I.see( 'test description', '.about-description');
 });
+
 
 
 
