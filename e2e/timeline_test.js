23786b6eaa567b111217401bf29f251d8dd27dd4
Christophe Theyssen <christophe.theyssen@ordina.be>
2016-04-13 16:34:20 +0200

    E2e test for timeline

--- e2e/timeline_test.js
+++ e2e/timeline_test.js
@@ -8,8 +8,11 @@ Scenario('Show the timeline and click on a card', (I) => {
   I.seeElement('timeline');
   I.waitForElement('#timeline', 2);
   I.see('Timeline', '#timeline');
-  I.dontSeeElement('.btnEdit', '#timeline');
-
+  I.dontSeeElement('#timeline .btnEdit');
+  I.seeElement('.fade-text', '#timeline');
+  I.dontSee('Personal motivation', '#timeline');
+  I.click('.timeline-card', '#timeline');
+  I.see('Personal motivation', '#timeline');
 });
 
 
