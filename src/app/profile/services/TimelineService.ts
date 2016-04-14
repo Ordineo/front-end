1ddba9ece1dfd569a7a85a184f9c228529d9a308
Christophe Theyssen <christophe.theyssen@ordina.be>
2016-04-14 11:51:13 +0200

    linked service data to timelineservice

--- src/app/profile/services/TimelineService.ts
+++ src/app/profile/services/TimelineService.ts
@@ -1,23 +1,37 @@
 import IPromise = angular.IPromise;
 import IHttpService = angular.IHttpService;
 import IQService = angular.IQService;
+import {GatewayApiService} from "../../gateway/service/GatewayApiService";
+import {TraversonHalService} from "../../traverson/service/TraversonHalService";
 var mock:any = require('./mocktimelinedata.json');
 
 export class TimelineService {
 
   static NAME:string = "TimelineService";
-  static $inject = ['$http', '$q'];
+  static $inject:Array<string> = [
+    TraversonHalService.SERVICE_NAME,
+    GatewayApiService.SERVICE_NAME,
+    '$q',
+    '$http'];
 
-  constructor(private httpService:IHttpService, private qService:IQService){
+  constructor(private traverson:TraversonHalService,
+              private gateway:GatewayApiService,
+              private $q:IQService,
+              private $http:IHttpService){
   }
 
-  public getTimelineByUsername(username:string):IPromise<any>{
-    var deffered = this.qService.defer();
-
+  public getMilestonesByUsername(userName:string):IPromise<any> {
+    return this.traverson.hal()
+      .from(this.gateway.getMilestonesApi())
+      .jsonHal()
+      .follow('findByUsername','milestones[$all]')
+      .withTemplateParameters({username: userName})
+      .getResource()
+      .result;
+    /*var deffered = this.$q.defer();
     deffered.resolve(
-      mock
+      this.$http.get(this.gateway.getMilestonesApi(username))
     );
-
-    return deffered.promise;
+    return deffered.promise;*/
   }
 }
