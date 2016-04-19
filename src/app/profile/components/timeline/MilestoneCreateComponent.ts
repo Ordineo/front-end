38fe99996cb6850b630c528c74b4a8e25cdc337c
Christophe Theyssen <Theyssen@users.noreply.github.com>
2016-04-19 09:03:36 +0200

    Milestone valid (#61)
    * Validation
    
    * Fix form invalid binding

--- src/app/profile/components/timeline/MilestoneCreateComponent.ts
+++ src/app/profile/components/timeline/MilestoneCreateComponent.ts
@@ -2,6 +2,7 @@ import IComponentOptions = angular.IComponentOptions;
 import IScope = angular.IScope;
 import IAngularEvent = angular.IAngularEvent;
 import {Milestone} from "../../../core/models/milestone";
+import IFormController = angular.IFormController;
 
 export class MilestoneCreateComponent implements IComponentOptions {
   static NAME:string = "milestoneCreate";
@@ -9,7 +10,8 @@ export class MilestoneCreateComponent implements IComponentOptions {
   template:string = require('./MilestoneCreate-template.html');
   bindings:any = {
     username: '@',
-    onContentLoaded: '&'
+    onContentLoaded: '&',
+    isSaveEnabled: '&'
   };
 }
 
@@ -17,14 +19,25 @@ export class MilestoneCreateController {
   public title:string = "MilestoneCreate";
   public username:string;
   public onContentLoaded:Function;
+  public isSaveEnabled:Function;
+  public minDate:Date;
 
   public milestone:Milestone;
 
-  constructor() {
+  static $inject = ['$scope'];
+
+  constructor(private scope:IScope) {
     this.milestone = <Milestone>{};
+    this.milestone.createDate = new Date();
+    this.milestone.dueDate = new Date();
+    this.minDate = new Date();
   }
+
   $onInit():void {
     this.onContentLoaded({isLoaded: true});
+    this.scope.$watch('milestoneCreateForm.$valid', ()=> {
+      this.isSaveEnabled({isEnabled: this.scope['milestoneCreateForm'].$valid});
+    });
   }
 
   private save():void {
