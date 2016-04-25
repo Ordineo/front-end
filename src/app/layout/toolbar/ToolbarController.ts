b7738150c572de1bf5d8806fd8e02a23179a1f67
Jasper Biesemans <jasper.biesemans@ordina.be>
2016-04-25 07:46:45 +0200

    Implement search functionality

--- src/app/layout/toolbar/ToolbarController.ts
+++ src/app/layout/toolbar/ToolbarController.ts
@@ -1,7 +1,14 @@
-
+import {ProfileService} from "../../profile/services/ProfileService";
+import {Employee} from "../../core/models/employee";
 import IRootScopeService = angular.IRootScopeService;
+import IObservable = Rx.IObservable;
 import {AboutDirectiveController} from "../../profile/components/about/AboutDirectiveController";
+export interface User{
+  value:string;
+  display:string;
+}
 export interface IToolbarScope{
+  users:Array<User>;
   buttons:Array<ButtonConfig>;
 }
 interface ButtonConfig{
@@ -9,17 +16,19 @@ interface ButtonConfig{
   icon:string;
 }
 export class ToolbarController implements IToolbarScope{
+  public users:Array<User>;
   public buttons:Array<ButtonConfig> = [];
   originatorEv:any;
 
   firstName:string;
   lastName:string;
 
-  static $inject = ['$rootScope'];
+  static EVENT_USER_SELECTED:string = "searchUserSelected";
+  static $inject = [ProfileService.NAME, '$rootScope'];
 
-  constructor(private $rootScope:IRootScopeService){
+  constructor(private profileService:ProfileService, private rootScope:IRootScopeService){
     this.buttons = this.getButtons();
-    $rootScope.$on(AboutDirectiveController.EVENT_ON_EMPLOYEEDATA_SET,(event, args)=>{
+    rootScope.$on(AboutDirectiveController.EVENT_ON_EMPLOYEEDATA_SET,(event, args)=>{
       this.firstName = args.firstName;
       this.lastName = args.lastName;
     });
@@ -45,5 +54,40 @@ export class ToolbarController implements IToolbarScope{
   private openMenu($mdOpenMenu, ev) {
     this.originatorEv = ev;
     $mdOpenMenu(ev);
+  }
+
+  selectedItemChange(_user_:User):void{
+    if(_user_){
+      this.rootScope.$broadcast(ToolbarController.EVENT_USER_SELECTED, {username: _user_.value});
+    }
+  }
+
+  querySearch(query:any):any{
+    return query ? this.users.filter(this.filter(query)) : this.users;
+  }
+
+  filter(query):any{
+    return (user:any)=>{
+      var lowerCaseUserDisplay:string = user.display.toLocaleLowerCase().trim();
+      return lowerCaseUserDisplay.indexOf(query) !== -1;
+    }
+  }
+
+  $onInit():void {
+    this.profileService.getAllEmployees()
+      .then((employees:Array<Employee>)=> {
+        this.users = this.parseEmployees(employees);
+      });
+  }
+
+  private parseEmployees(employees:Array<Employee>):Array<User>{
+    var users:Array<User> = [];
+    for (var emp of employees) {
+      users.push({
+        value: emp.username,
+        display: emp.firstName + ' ' + emp.lastName
+      });
+    }
+    return users;
   }
 }
