1c3357f98a0c2683d94b53fc49198c8eb5f3dced
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2016-05-10 10:55:39 +0200

    Added method to MockProfileService

--- test/mock/MockProfileService.ts
+++ test/mock/MockProfileService.ts
@@ -3,6 +3,9 @@ import {Employee} from "../../src/app/core/models/employee";
 import {IProfileService} from "../../src/app/profile/services/ProfileService";
 
 export class MockProfileService implements IProfileService {
+  getBasicInfoByUsername(userName:string):angular.IPromise<any> {
+    return undefined;
+  }
 
   getAboutInfoByUsername(username:string):IPromise<any> {
     return null;
