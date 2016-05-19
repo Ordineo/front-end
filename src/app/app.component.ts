c8a301df3fffc6502415e75fcfad8499cc2c70c0
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-05-20 00:46:20 +0200

    refactor(tslint): Reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
 import IComponentOptions = angular.IComponentOptions;
-import Router = angular.Router;
 import RouteDefinition = angular.RouteDefinition;
-import {MainRoute, DashboardRoute, LoginRoute} from "./app.routes";
+import {MainRoute, DashboardRoute, LoginRoute} from './app.routes';
 import './app-component.scss';
-import {AuthService, IAuthService} from "./auth/service/AuthService";
-import {DashboardRoutes} from "./layout/DashboardComponent";
-import {SessionService} from "./auth/service/SessionService";
-import {ProfileService} from "./profile/services/ProfileService";
+import {AuthService, IAuthService} from './auth/service/AuthService';
+import {DashboardRoutes} from './layout/DashboardComponent';
+import {SessionService} from './auth/service/SessionService';
+import {ProfileService} from './profile/services/ProfileService';
 
 export class AppComponent implements IComponentOptions {
-  static NAME:string = "app";
+  static NAME: string = 'app';
 
-  controller:Function = AppComponentController;
-  template:string = require('./app-component.html');
-  $routeConfig:RouteDefinition[] = [
+  controller: Function = AppComponentController;
+  template: string = require('./app-component.html');
+  $routeConfig: RouteDefinition[] = [
     new MainRoute(),
     new DashboardRoute(),
     new LoginRoute()
@@ -22,10 +21,12 @@ export class AppComponent implements IComponentOptions {
 export class AppComponentController {
   static $inject = [AuthService.NAME, ProfileService.NAME, SessionService.NAME];
 
-  constructor(private authService:IAuthService, private profileService:ProfileService, private sessionService:SessionService) {
+  constructor(private authService: IAuthService,
+              private profileService: ProfileService,
+              private sessionService: SessionService) {
   }
 
-  $routerOnActivate(next:any, previous:any):void {
+  $routerOnActivate(next: any, previous: any): void {
     if (next.params.username !== undefined) {
       this.profileService.setUsername(next.params.username);
     } else {
