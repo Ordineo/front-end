d89038981997f1fdd9cffc87851f134fb3f28c45
Ryan De Gruyter <degruyter.ryan@gmail.com>
2016-05-20 00:33:32 +0200

    refactor(tslint): replace single double quotes with single quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
-import {THEME_MODULE} from "./theme/ThemeModule";
-import {AppComponent} from "./app.component.ts";
-import {ORDINEO_CORE} from "./core/core.module.ts";
-import {ORDINEO_PROFILE} from "./profile/ProfileModule.ts";
-import {ORDINEO_LAYOUT} from "./layout/LayoutModule";
-import {ORDINEO_GATEWAY} from "./gateway/GatewayModule";
-import {ORDINEO_SOCIAL} from "./social/SocialModule";
-import IHttpProvider = angular.IHttpProvider;
-import {JWORKS_AUTH} from "./auth/AuthModule";
+import {THEME_MODULE} from './theme/ThemeModule';
+import {AppComponent} from './app.component.ts';
+import {ORDINEO_CORE} from './core/core.module.ts';
+import {ORDINEO_PROFILE} from './profile/ProfileModule.ts';
+import {ORDINEO_LAYOUT} from './layout/LayoutModule';
+import {ORDINEO_GATEWAY} from './gateway/GatewayModule';
+import {ORDINEO_SOCIAL} from './social/SocialModule';
+import {JWORKS_AUTH} from './auth/AuthModule';
 
-export const APP_NAME = "ordineo";
+export const APP_NAME = 'ordineo';
 
 angular
   .module(APP_NAME, [
