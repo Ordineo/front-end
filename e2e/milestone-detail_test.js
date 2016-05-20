263138d02320e3452ebf8ab50703e947f28beac0
Christophe Theyssen <christophe.theyssen@ordina.be>
2016-05-20 14:09:07 +0200

    Added e2e test for milestone done.

--- e2e/milestone-detail_test.js
+++ e2e/milestone-detail_test.js
@@ -12,7 +12,7 @@ Scenario('Show the details of a milestone', (I) => {
   I.see('Please select a milestone', '#milestone-details');
   I.see('Spring Boot', '#milestone-list');
   I.click('//span[text()="Spring Boot "]', '#milestone-list');
-  I.see('Spring Boot', '#milestone-details');
+  I.see('Spring Boot', '#milestone-details'); 
 });
 
 Scenario('Edit the details of a milestone', (I) => {
@@ -23,6 +23,25 @@ Scenario('Edit the details of a milestone', (I) => {
   I.fillField({model: '$ctrl.milestone.moreInformation'},'More information test');
   I.click('.btnOk', '#edit-milestone');
   I.see('More information test', '#milestone-details');
+  I.click('.btnEdit', '#milestone-details');
+  I.seeElement('#edit-milestone');
+  I.fillField({model: '$ctrl.milestone.moreInformation'},'More information');
+  I.click('.btnOk', '#edit-milestone');
+  I.dontSee('More information test', '#milestone-details');
+});
+
+Scenario('Mark a milestone as done', (I) => {
+  I.click('//span[text()="Spring Boot "]', '#milestone-list');
+  I.click('.btnEdit', '#milestone-details');
+  I.seeElement('#edit-milestone');
+  I.click('.checkAccomplished', '#edit-milestone');
+  I.click('.btnOk', '#edit-milestone');
+  I.see('Accomplished', '#milestone-details');
+  I.click('.btnEdit', '#milestone-details');
+  I.seeElement('#edit-milestone');
+  I.click('.checkAccomplished', '#edit-milestone');
+  I.click('.btnOk', '#edit-milestone');
+  I.dontSee('Accomplished', '#milestone-details');
 });
 
 
