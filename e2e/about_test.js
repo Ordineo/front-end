ce292cad61d156b46e0a568f25aa971b34bdc79a
Christophe Theyssen <christophe.theyssen@ordina.be>
2016-05-20 13:31:49 +0200

    E2e added for showing the username

--- e2e/about_test.js
+++ e2e/about_test.js
@@ -9,6 +9,7 @@ Before((I) => {
 
 Scenario('Open the profile page', (I) => {
   I.see('JWorks 360', '#toolbar');
+  I.see('Test Tester', 'app-header');
   //I.see('Sync with Linkedin', '#getDetails'); //todo show linkdin
   I.waitForElement('.about-header', 2);
   I.seeElement('.about-header');
