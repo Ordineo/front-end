c11fd5ffaf3a0c2c4557121b083aa0b1cf19500d
Jasper Biesemans <jasper.biesemans@ordina.be>
2016-05-20 12:52:41 +0200

    Component config

---
+++ src/app/core/components/notifications/NotificationsController.ts
@@ -0,0 +1,17 @@
+import IRootScopeService = angular.IRootScopeService;
+import IAngularEvent = angular.IAngularEvent;
+import ISCEService = angular.ISCEService;
+import IScope = angular.IScope;
+var $ = require('jquery');
+
+export class NotificationsController {
+  
+
+  static $inject:Array<string> = [
+    
+  ];
+
+  constructor() {
+    
+  }
+} 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -14,6 +14,7 @@ import {TRAVERSON} from "../traverson/TraversonModule";
 import {AppComponent} from "../app.component";
 import {SessionService} from "../auth/service/SessionService";
 import {ProfileSearchComponent} from "./components/profile-search/ProfileSearchComponent";
+import {NotificationsComponent} from "./components/notifications/NotificationsComponent";
 import {Navigator} from "./services/Navigator";
 import IScope = angular.IScope;
 import IAugmentedJQuery = angular.IAugmentedJQuery;
@@ -35,6 +36,7 @@ angular.module(ORDINEO_CORE, deps)
   .component(CardHeaderComponent.NAME, new CardHeaderComponent())
   .component(ActionButtonComponent.NAME, new ActionButtonComponent())
   .component(ProfileSearchComponent.NAME, new ProfileSearchComponent())
+  .component(NotificationsComponent.NAME, new NotificationsComponent())
   .service(SessionService.NAME, SessionService)
   .service(Navigator.NAME, Navigator)
   .directive(FileUploadDirective.NAME, FileUploadDirective.instance())
