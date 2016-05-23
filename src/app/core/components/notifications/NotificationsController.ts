fc6a256b5e338c876cc3c1362dd94248d6d78021
Jasper Biesemans <jasper.biesemans@ordina.be>
2016-05-23 08:00:30 +0200

    Added notification menu

--- src/app/core/components/notifications/NotificationsController.ts
+++ src/app/core/components/notifications/NotificationsController.ts
@@ -4,14 +4,44 @@ import ISCEService = angular.ISCEService;
 import IScope = angular.IScope;
 var $ = require('jquery');
 
+interface ButtonConfig {
+  title:string;
+  icon:string;
+}
+
 export class NotificationsController {
-  
+  public buttons:Array<ButtonConfig> = [];
+  originatorEv:any;
 
   static $inject:Array<string> = [
-    
+
   ];
 
   constructor() {
+    this.buttons = this.getButtons();
+  }
+
+  private getButtons():Array<ButtonConfig> {
+    var btns:Array<ButtonConfig> = [];
+
+    btns.push(this.createButtonConfig('notifications', 'social:notifications'));
+
+    return btns;
+  }
+
+  private createButtonConfig(_title:string, _icon:string):ButtonConfig {
+    return {
+      title: _title,
+      icon: _icon
+    };
+  }
+
+  public openMenu($mdOpenMenu, ev) {
+    this.originatorEv = ev;
+    $mdOpenMenu(ev);
+  }
+
+  private goToNotification():void {
     
   }
 } 
