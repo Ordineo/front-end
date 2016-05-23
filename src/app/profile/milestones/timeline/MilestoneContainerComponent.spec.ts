d600e7de00877026f4409c2113f075249b4e4dda
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2016-05-23 14:35:48 +0200

    MilestoneContainerComponent tests
    These tests throw errors because they access private members but run properly...strange...

--- src/app/profile/milestones/timeline/MilestoneContainerComponent.spec.ts
+++ src/app/profile/milestones/timeline/MilestoneContainerComponent.spec.ts
@@ -10,7 +10,7 @@ import IProvideService = angular.auto.IProvideService;
 import IRootScopeService = angular.IRootScopeService;
 import IScope = angular.IScope;
 
-describe("describe text", ()=> {
+describe("MilestoneContainerController", ()=> {
   var scope:IScope;
   var ctrl:MilestoneContainerController;
   var milestoneService:IMilestoneService;
@@ -32,12 +32,71 @@ describe("describe text", ()=> {
     ctrl = _$componentController_(MilestoneContainerComponent.NAME, {$scope: scope});
   }));
 
-  describe("onContentLoaded", ()=> {
+  describe("on init", ()=> {
 
-    it("test", ()=> {
-      console.log("logtest");
+    it("configures card header buttons", ()=> {
+      spyOn(ctrl, "configureCardHeaderButtons");
+      ctrl.$onInit();
+      expect(ctrl.configureCardHeaderButtons).toHaveBeenCalled();
     });
 
+    it("actionbuttons should not be empty", ()=> {
+      ctrl.$onInit();
+      expect(ctrl.actionButtons).not.toEqual([]);
+    });
+
+    it("initialises create button", ()=> {
+      ctrl.$onInit();
+      var btnCreate = 'btnCreate';
+      var button = ctrl.actionButtons.find(obj => obj.aClass === btnCreate);
+      expect(button.aClass).toEqual(btnCreate);
+    });
+
+    it("initialises save button", ()=> {
+      ctrl.$onInit();
+      var btnSave = 'btnSave';
+      var button = ctrl.actionButtons.find(obj => obj.aClass === btnSave);
+      expect(button.aClass).toEqual(btnSave);
+    });
+
+    it("initialises cancel button", ()=> {
+      ctrl.$onInit();
+      var btnCancel = 'btnCancel';
+      var button = ctrl.actionButtons.find(obj => obj.aClass === btnCancel);
+      expect(button.aClass).toEqual(btnCancel);
+    });
+
+  });
+
+  describe("onContentLoaded", ()=> {
+
+    it("when given true, isContentLoaded should be true", ()=> {
+      ctrl.onContentLoaded(true);
+      expect(ctrl.isContentLoaded).toBeTruthy();
+    });
+
+    it("when given false, isContentLoaded should be false", ()=> {
+      ctrl.onContentLoaded(false);
+      expect(ctrl.isContentLoaded).toBeFalsy();
+    });
+
+  });
+
+  describe("isSaveEnabled", ()=> {
+
+    it("is true, savebutton should not be disabled", ()=> {
+      ctrl.$onInit();
+      ctrl.isSaveEnabled(true);
+      var button = ctrl.saveButton;
+      expect(button.isDisabled).toBeFalsy();
+    });
+
+    it("is false, savebutton should be disabled", ()=> {
+      ctrl.$onInit();
+      ctrl.isSaveEnabled(false);
+      var button = ctrl.saveButton;
+      expect(button.isDisabled).toBeTruthy();
+    });
   });
 
 });
